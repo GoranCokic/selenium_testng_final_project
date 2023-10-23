549387b6ca2cc95a16b56c3b96a4e4d6a9d33882
GoranCokic <gorancokic@protonmail.com>
2023-10-23 19:33:30 +0200

    TC_ART_04 - Forbids visits to admin users url if not authenticated

--- src/test/java/tests/AuthRoutesTests.java
+++ src/test/java/tests/AuthRoutesTests.java
@@ -8,7 +8,7 @@ public class AuthRoutesTests extends BasicTest {
 
     @Test(priority = 1, retryAnalyzer = RetryTests.class)
     public void forbidsVisitsToHomeUrlIfNotAuthenticated() {
-        String urlExtendWrong ="/home";
+        String urlExtendWrong = "/home";
         String urlExtendCorrect = "/login";
         driver.navigate().to(baseUrl + urlExtendWrong);
         wait
@@ -18,7 +18,7 @@ public class AuthRoutesTests extends BasicTest {
 
     @Test(priority = 2, retryAnalyzer = RetryTests.class)
     public void forbidsVisitsToProfileUrlIfNotAuthenticated() {
-        String urlExtendWrong ="/profile";
+        String urlExtendWrong = "/profile";
         String urlExtendCorrect = "/login";
         driver.navigate().to(baseUrl + urlExtendWrong);
         wait
@@ -28,7 +28,17 @@ public class AuthRoutesTests extends BasicTest {
 
     @Test(priority = 3, retryAnalyzer = RetryTests.class)
     public void forbidsVisitsToAdminCitiesUrlIfNotAuthenticated() {
-        String urlExtendWrong ="/admin/cities";
+        String urlExtendWrong = "/admin/cities";
+        String urlExtendCorrect = "/login";
+        driver.navigate().to(baseUrl + urlExtendWrong);
+        wait
+                .withMessage("|||Url should be" + baseUrl + urlExtendCorrect + " but is " + baseUrl + urlExtendWrong)
+                .until(ExpectedConditions.urlToBe(baseUrl + urlExtendCorrect));
+    }
+
+    @Test(priority = 4, retryAnalyzer = RetryTests.class)
+    public void forbidsVisitsToAdminUsersUrlIfNotAuthenticated() {
+        String urlExtendWrong = "/admin/users";
         String urlExtendCorrect = "/login";
         driver.navigate().to(baseUrl + urlExtendWrong);
         wait
